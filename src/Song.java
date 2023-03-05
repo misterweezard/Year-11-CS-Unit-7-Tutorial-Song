5cf565affb5a7eda821b2a41270a870baa4a230c
misterweezard <ksc029@student.cis.edu.jk>
2023-03-05 21:04:48 +0800

    ok

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Song {
 
-    private String
+    private final String title;
+    private final String artist;
+
+    public Song(String x, String y) {
+        title = x;
+        artist = y;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+    private static final ArrayList<String> a = new ArrayList<>();
+    public static int howMany(ArrayList<String> arr) {
+        int x = 0;
+        for (String s : arr) {
+            String name = s.toLowerCase();
+            if (!a.contains(name)) {
+                a.add(name);
+                x++;
+            }
+        }
+        return(x);
+    }
+
+
 }
